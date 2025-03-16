d618e5f9386fb36a60249c3386ca5173736e8aa1
mishkajackson <92293134+mishkajackson@users.noreply.github.com>
2025-03-16 11:01:00 +0300

    Update getip.js

--- Stash/Scripts/getip.js
+++ Stash/Scripts/getip.js
@@ -1,18 +1,20 @@
 $httpClient.get('https://ipapi.co/json/', function (error, response, data) {
   if (error) {
-    $done({ title: 'IP', content: 'Error fetching data', backgroundColor: '#ff0000' });
+    $done({ title: 'Your IP', content: 'Error fetching data', backgroundColor: '#ff0000' });
   } else {
     let info = JSON.parse(data);
+    
+    // Выполняем ping до Cloudflare
     let startTime = Date.now();
     $httpClient.get('https://1.1.1.1/cdn-cgi/trace', function (pingError, pingResponse, pingData) {
       let pingTime = pingError ? 'N/A' : (Date.now() - startTime) + ' ms';
       
-      let content = `${info.ip}\n${info.city}\n${info.org}\n${pingTime}`;
+      let content = `IP: ${info.ip}\n${info.city}\nISP: ${info.org}\nPing: ${pingTime}`;
       
       $done({
         title: 'Информация',
         content: content,
-        backgroundColor: '#696aad',
+        backgroundColor: '#178BD8',
         icon: 'network',
       });
     });
