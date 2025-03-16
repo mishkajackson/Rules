51cd5dc976e185b8913c8750b54dd7f655cd1987
mishkajackson <92293134+mishkajackson@users.noreply.github.com>
2025-03-16 09:59:17 +0300

    Update getip.js

--- Stash/Scripts/getip.js
+++ Stash/Scripts/getip.js
@@ -9,25 +9,26 @@ $httpClient.get('https://ipapi.co/json/', function (error, response, data) {
     $httpClient.get('https://1.1.1.1/cdn-cgi/trace', function (pingError, pingResponse, pingData) {
       let pingTime = pingError ? 'N/A' : (Date.now() - startTime) + ' ms';
 
-      // Проверяем доступность сайтов
+      // Проверяем пинг до сайтов
       let sites = {
         'YouTube': 'https://www.youtube.com',
         'Instagram': 'https://www.instagram.com',
         'OpenAI': 'https://openai.com'
       };
-      
+
       let statuses = [];
       let checked = 0;
 
       Object.keys(sites).forEach(site => {
+        let siteStartTime = Date.now();
         $httpClient.get(sites[site], function (siteError) {
-          let status = siteError ? '❌' : '✅';
-          statuses.push(`${site}: ${status}`);
+          let sitePing = siteError ? 'N/A' : (Date.now() - siteStartTime) + ' ms';
+          statuses.push(`${site}: ${sitePing}`);
           checked++;
 
           // Когда все сайты проверены, формируем контент и завершаем выполнение
           if (checked === Object.keys(sites).length) {
-            let content = `${info.ip}\n${info.city}\n${info.org}\nPing: ${pingTime}\n\n` + statuses.join('\n');
+            let content = `${info.ip}\n${info.city}\n${info.org}\nCloudflare: ${pingTime}\n\n` + statuses.join('\n');
             
             $done({
               title: 'Информация',
